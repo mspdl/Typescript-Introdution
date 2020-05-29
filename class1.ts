246409b380733759c68d30061f388f48612785a6
Morgan Espindola <morgan.espindola@gmail.com>
2020-05-28 22:14:04 -0300

    First Class

---
+++ class1.ts
@@ -0,0 +1,22 @@
+let message: string = "Help me, young one."
+console.log(message)
+
+let page = 1
+console.log("This is page " + page + ".")
+page += 1
+console.log("Next page is " + page + ".")
+
+let favoriteColor
+favoriteColor = 'Blue'
+console.log("My favorite color is " + favoriteColor)
+
+// Class 1 extended:
+let firstName = "Morgan"
+let age
+age = 30
+let lastName: string = "Espindola"
+let classNumber = 1
+console.log("------------ My own code practice ------------")
+console.log("My name is " + firstName + " " + lastName + "."
+    + " I'm " + age + " years old."
+    + " And this is class number " + classNumber + ".")
